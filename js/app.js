c9989cc8fb7709a15a63b55078f33913294a63ba
commonstarling <sheenaramirez2@gmail.com>
2015-11-04 17:06:50 -0600

    submit functionality

--- js/app.js
+++ js/app.js
@@ -1,94 +1,61 @@
+
 $(document).ready(function(){
-     
-    /*--- Display information modal box ---*/
-	$(".what").click(function(){
-		$(".overlay").fadeIn(1000);  
-	});
- 
-    /*--- Hide information modal box ---*/
-    $("a.close").click(function(){
-        $(".overlay").fadeOut(1000);
-	});
-});
+	
+	/*--- Display information modal box ---*/
+  	$(".what").click(function(){
+    	$(".overlay").fadeIn(1000);
+
+  	});
+
+  	/*--- Hide information modal box ---*/
+  	$("a.close").click(function(){
+  		$(".overlay").fadeOut(1000);
+  	});
+
 /*function to generate random number*/
-function secretNumberGenerator(){
-             secretNumber = (Math.floor(Math.random()*100));
-                 console.log("Secret Number = " + secretNumber);
+	function secretNumberGenerator() {
+    	secretNumber = (Math.floor(Math.random()*100));
+        	console.log("Secret Number = " + secretNumber);
+	}
+	secretNumberGenerator();
+
+/*function to submit form*/
+function submitForm() {
+$('form').on('submit', function(e) {
+  e.preventDefault();
+  var userGuess = $('#userGuess').val();
+  console.log("User guess is " + userGuess);
+  validateNumber(userGuess);
+  resetForm();
+})
 }
-secretNumberGenerator();
+submitForm();
 
-/*function to capture input number*/
-$('#guessButton').click(function() {
-        	var guessedNumber = $('#userGuess').val();
-        	var guessedNumberInt = parseInt(guessedNumber, 10);
-        	alert(guessedNumberInt);
-        	hotCold(guessedNumberInt);
-            resetForm();
-});
-
-/*function to reset input box*/
+/*function to clear input after typing number*/
 function resetForm() {
 	$('#userGuess').val('');
 }
 
-/*function to tell user if they're hot or cold*/
-function setFeedback(feedback) {
-             $("#feedback").text(feedback);
-             }
-
-/*function to compare user guess with secret number*/
-function hotCold() {
-	function positiveValue() {
-		if (guessedNumberInt === secretNumber) {
-			setFeedback("Congrats, you got it!");
+/*function to check if number is valid */
+function validateNumber() {
+	var userGuess = $('#userGuess').val();
+	var userGuessInt = parseInt(userGuess);
+		if (typeof userGuessInt !== 'number') {
+			alert("Please submit an integer from 1 to 100");
 		}
-		else if ((guessedNumberInt - secretNumber) >= 50) {
-			setFeedback("Brrrr... you're freezing!");
+		else if (userGuessInt === NaN) {
+			alert("Please submit an integer from 1 to 100");
 		}
-		else if ((guessedNumberInt - secretNumber) > 40) {
-			setFeedback("You're warmer, but still pretty cold!");
+		else if (userGuessInt > 100) {
+			alert("Please submit a valid integer from 1 to 100");
 		}
-		else if ((guessedNumberInt - secretNumber) > 30) {
-			setFeedback("The heat's starting to kick in!");
+		else if (userGuessInt < 0) {
+			alert("Please submit a valid integer from 1 to 100");
 		}
-		else if ((guessedNumberInt - secretNumber) > 20) {
-			setFeedback("Now we're getting toasty!");
-		}
-		else if ((guessedNumberInt - secretNumber) > 10) {
-			setFeedback("Whew, it's so hot I'm breaking a sweat!");
-		}
-		else if ((guessedNumberInt - secretNumber) > 5) {
-			setFeedback("You're burning up!");
-		}
-	}
-
-	function negativeValue() {
-		if (guessedNumberInt === secretNumber) {
-			setFeedback("Congrats, you got it!");
-		}
-		else if ((guessedNumberInt - secretNumber) <= -50) {
-			setFeedback("Brrrr... you're freezing!");
-		}
-		else if ((guessedNumberInt - secretNumber) < -40) {
-			setFeedback("You're warmer, but still pretty cold!");
-		}
-		else if ((guessedNumberInt - secretNumber) < -30) {
-			setFeedback("The heat's starting to kick in!");
-		}
-		else if ((guessedNumberInt - secretNumber) < -20) {
-			setFeedback("Now we're getting toasty!");
-		}
-		else if ((guessedNumberInt - secretNumber) < -10) {
-			setFeedback("It's so hot, I'm breaking a sweat!");
-		}
-		else if ((guessedNumberInt - secretNumber) < -5) {
-			setFeedback("You're burning up!");
-		}
-	}
+		/*else {
+			hotCold(userGuessInt);
+		}*/
 }
 
-/*function to track previous guesses*/
 
-/*function to track number of guesses*/
-
-/*function to reset game*/
+});

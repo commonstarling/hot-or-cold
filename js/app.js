deeb4e0794efe67f55d6c2b572b25f8bbb88a478
commonstarling <sheenaramirez2@gmail.com>
2015-11-05 14:36:29 -0600

    number comparison finalization

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*Global variable list*/
 var userGuessInt = '';
-var secretNumberGenerator = '';
+var secretNumber = '';
 
 $(document).ready(function(){
 	
@@ -41,7 +41,7 @@ function resetForm() {
 /*function to check if number is valid */
 function validateNumber() {
 	var userGuess = $('#userGuess').val();
-	var userGuessInt = parseInt(userGuess, 10);
+	var userGuessInt = parseInt(userGuess);
 		if (isNaN(userGuessInt) === true) {
 			alert("Please submit an integer from 1 to 100");
 		}
@@ -52,20 +52,23 @@ function validateNumber() {
 			alert("Please submit a valid integer from 1 to 100");
 		}
 		else {
-			hotCold();
+			hotCold(userGuessInt);
 		}
 }
 
 /*function to weigh if guesses are hot or cold*/
-function hotCold() {
-	if (userGuessInt === secretNumber) {
+function hotCold(userGuessInt) {
+	var userGuess = $('#userGuess').val();
+	var userGuessInt = parseInt(userGuess);
+	var difference = (userGuessInt - secretNumber);
+	if (difference === 0) {
 		setFeedback("You win!");
 	}
-	else if	((userGuessInt - secretNumber) < 0) {
-		negativeValue(userGuessInt);
+	else if	(difference > 0) {
+		positiveValue(userGuessInt);
 	}
 	else {
-		positiveValue(userGuessInt);
+		negativeValue(userGuessInt);
 	}
 }
 
@@ -77,52 +80,59 @@ function setFeedback(feedback) {
 
 /*function to consider numbers higher than the secret number*/
 function positiveValue() {
-		if (userGuessInt == secretNumber) {
-			setFeedback("You win!");
-		}
-		else if ((userGuessInt - secretNumber) <= 5) {
-			setFeedback("You're burning up!");
-		}
-		else if ((userGuessInt - secretNumber) < 10) {
-			setFeedback("Whew, it's so hot I'm breaking a sweat!");
-		}
-		else if ((userGuessInt - secretNumber) < 20) {
-			setFeedback("Now we're getting toasty!");
-		}
-		else if ((userGuessInt - secretNumber) < 30) {
-			setFeedback("The heat's starting to kick in!");
-		}
-		else if ((userGuessInt - secretNumber) < 40) {
-			setFeedback("You're warmer, but still pretty cold!");
-		}
-		else if ((userGuessInt - secretNumber) > 50) {
-			setFeedback("Brrr... You're freezing!");
-		}
+	var userGuess = $('#userGuess').val();
+	var userGuessInt = parseInt(userGuess);
+	var posDifference = (userGuessInt - secretNumber);
+	if (posDifference <= 5) {
+		setFeedback("You're burning up!");
 	}
+	else if (posDifference <= 10) {
+		setFeedback("Whew, it's so hot I'm breaking a sweat!");
+	}
+	else if (posDifference <= 20) {
+		setFeedback("Now we're getting toasty!");
+	}
+	else if (posDifference <= 30) {
+		setFeedback("The heat's starting to kick in!");
+	}
+	else if (posDifference <= 40) {
+		setFeedback("You're warmer, but still pretty cold!");
+	}
+	else if (posDifference <= 50) {
+		setFeedback("Brrr... You're freezing!");
+	}
+	else if (posDifference > 50) {
+		setFeedback("You're in the North Pole!");
+	}
+}
+
 
 /*function to consider numbers lower than the secret number*/
 function negativeValue() {
-	if (userGuessInt === secretNumber) {
-			setFeedback("You win!");
-		}
-		else if ((secretNumber - userGuessInt) <= 5) {
-			setFeedback("You're burning up!");
-		}
-		else if ((secretNumber - userGuessInt) < 10) {
-			setFeedback("Whew, it's so hot I'm breaking a sweat!");
-		}
-		else if ((secretNumber - userGuessInt) < 20) {
-			setFeedback("Now we're getting toasty!");
-		}
-		else if ((secretNumber - userGuessInt) < 30) {
-			setFeedback("The heat's starting to kick in!");
-		}
-		else if ((secretNumber - userGuessInt) < 40) {
-			setFeedback("You're warmer, but still pretty cold!");
-		}
-		else if ((secretNumber - userGuessInt) >= 50) {
-			setFeedback("Brrr... You're freezing!");
-		}
+	var userGuess = $('#userGuess').val();
+	var userGuessInt = parseInt(userGuess);
+	var negDifference = (secretNumber - userGuessInt);
+	if (negDifference <= 5) {
+		setFeedback("You're burning up!");
 	}
+	else if (negDifference <= 10) {
+		setFeedback("Whew, it's so hot I'm breaking a sweat!");
+	}
+	else if (negDifference <= 20) {
+		setFeedback("Now we're getting toasty!");
+	}
+	else if (negDifference <= 30) {
+		setFeedback("The heat's starting to kick in!");
+	}
+	else if (negDifference <= 40) {
+		setFeedback("You're warmer, but still pretty cold!");
+	}
+	else if (negDifference <= 50) {
+		setFeedback("Brrr... You're freezing!");
+	}
+	else if (negDifference > 50) {
+		setFeedback("You're in the North Pole!");
+	}
+}
 
 });
